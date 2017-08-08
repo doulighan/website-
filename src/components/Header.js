858e81cca5d9f45dfd61a509d68d0c87dd3e50cd
doulighan <doulighan@gmail.com>
2017-08-08 15:32:14 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+
+  return (
+    <div className='header'>
+      <div className='header-titles'>
+        <h1>Devin's Website</h1>
+      </div>
+      <div className='header-actions'>
+        <button class='button'>Home</button>
+        <button class='button'>Other</button>
+      </div>
+    </div>
+  ) 
+}
+
+export default Header
